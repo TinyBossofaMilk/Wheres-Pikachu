b4d1fadc5fd9f0e62a71377b3bb82056fcb0a8a0
TinyBossofaMilk <ppcctan@gmail.com>
2023-01-13 02:06:24 -0800

    added countdown branch

--- src/App.js
+++ src/App.js
@@ -35,15 +35,15 @@ import { getFirebaseConfig } from './firebase-config.js';
 
 
 // TODO: Replace the following with your app's Firebase project configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyD4tf-yYxtqnRfo72mxqlVK1-f7aRD6weg",
-  authDomain: "wheres-pikachu-f0cf0.firebaseapp.com",
-  projectId: "wheres-pikachu-f0cf0",
-  storageBucket: "wheres-pikachu-f0cf0.appspot.com",
-  messagingSenderId: "145309171259",
-  appId: "1:145309171259:web:0ae21b8b077aadd26da61e",
-  measurementId: "G-FDPDYKH7WP"
-};
+// const firebaseConfig = {
+//   apiKey: "AIzaSyD4tf-yYxtqnRfo72mxqlVK1-f7aRD6weg",
+//   authDomain: "wheres-pikachu-f0cf0.firebaseapp.com",
+//   projectId: "wheres-pikachu-f0cf0",
+//   storageBucket: "wheres-pikachu-f0cf0.appspot.com",
+//   messagingSenderId: "145309171259",
+//   appId: "1:145309171259:web:0ae21b8b077aadd26da61e",
+//   measurementId: "G-FDPDYKH7WP"
+// };
 
 // const app = initializeApp(firebaseConfig);
 // const firebase = require("firebase");
